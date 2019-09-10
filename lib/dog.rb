e80a0f248b95e9a16fed8c52f49a23bbefd1a70d
Joseph Sesti <sestijoey63@gmail.com>
2019-09-10 17:24:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,12 +13,14 @@ class Dog
     @@all
   end
   
+  def self.print_all
+    puts @@all.map{|dog| dog.name
+  end
+  
   def self.clear_all
     @@all.clear
   end
   
-  def self.print_all
-    puts @@all.map{|dog| dog.name
-  end
+
   
 end
